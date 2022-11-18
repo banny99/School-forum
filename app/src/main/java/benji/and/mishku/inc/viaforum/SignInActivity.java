84e3d3e999e604e362031181cc671d1761a05dbd
banny99 <benjamin.bartosik@gmail.com>
2022-11-18 17:04:40 +0100

    email+pass registration [OK]

--- app/src/main/java/benji/and/mishku/inc/viaforum/SignInActivity.java
+++ app/src/main/java/benji/and/mishku/inc/viaforum/SignInActivity.java
@@ -1,14 +1,25 @@
 package benji.and.mishku.inc.viaforum;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.Arrays;
 import java.util.List;
 import benji.and.mishku.inc.viaforum.viewModels.SignInViewModel;
@@ -17,6 +28,10 @@ import benji.and.mishku.inc.viaforum.views.MainActivity;
 public class SignInActivity extends AppCompatActivity {
 
     private SignInViewModel viewModel;
+    private FirebaseAuth firebaseAuth;
+
+    private EditText email;
+    private EditText password;
 
     ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(), result -> {
@@ -32,6 +47,10 @@ public class SignInActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(SignInViewModel.class);
         checkIfSignedIn();
         setContentView(R.layout.activity_sign_in);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.signUp_email);
+        password = findViewById(R.id.signUp_Password);
     }
 
     private void checkIfSignedIn() {
@@ -59,5 +78,45 @@ public class SignInActivity extends AppCompatActivity {
                 .build();
 
         activityResultLauncher.launch(signInIntent);
+    }
+
+    public void register(View view) {
+        String userEmail = email.getText().toString().trim();
+        String userPassword = password.getText().toString().trim();
+
+        if (TextUtils.isEmpty(userEmail)) {
+            showToast("Enter email address!");
+            return;
+        }
+
+        if(TextUtils.isEmpty(userPassword)){
+            showToast("Enter Password!");
+            return;
+        }
+
+        if(userPassword.length() < 6){
+            showToast("Password too short, enter minimum 6 characters");
+            return;
+        }
+
+
+        //register user
+        firebaseAuth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "New user registration: " + task.isSuccessful());
+
+                        if (!task.isSuccessful()) {
+                            showToast("Authentication failed. " + task.getException());
+                        } else {
+                            goToMainActivity();
+                        }
+                    }
+                });
+    }
+
+    public void showToast(String toastText) {
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
     }
 }
